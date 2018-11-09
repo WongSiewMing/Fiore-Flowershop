a8bbcff512f4f12fb6f0fd4d2f8efbf1df9ddd7b
TARUC <TARUC@PC-97>
2018-11-09 10:45:55 +0800

    version 0.1

--- src/FioreFlowershop/FioreFlowershop.java
+++ src/FioreFlowershop/FioreFlowershop.java
@@ -6,6 +6,7 @@ public class FioreFlowershop {
     
     public static void main(String[] args) {
         System.out.print("Welcome to Fiore Flowershop !");
+        System.out.print("Please feel free to ask our staff for any help!");        
     }
     
 }
