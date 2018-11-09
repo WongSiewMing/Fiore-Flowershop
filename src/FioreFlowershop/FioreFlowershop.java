66a97d35cbe30e5f7c81a289bd8b11593a7c1677
TARUC <TARUC@PC-99>
2018-11-09 10:28:39 +0800

    Assignment QAQ

---
+++ src/FioreFlowershop/FioreFlowershop.java
@@ -0,0 +1,11 @@
+package FioreFlowershop;
+
+
+public class FioreFlowershop {
+
+    
+    public static void main(String[] args) {
+        System.out.print("Welcome to Fiore Flowershop !");
+    }
+    
+}
