474ed80ca54e54678c18dbdc460cec5217804b9c
TARUC <TARUC@PC-98>
2018-11-24 10:59:20 +0800

    new

---
+++ src/agileb/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package module.b;
+
+/**
+ *
+ * @author jun_y
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Frame1 f1 = new Frame1 ();
+        f1.setVisible(true);
+    }
+    
+}
