56db4a465cf4ea00c115d2bb39601eab42251c13
WongSiewMing <wongsm-wa16.student.tarc.edu.my>
2018-12-11 08:25:48 +0800

    version 3.5

---
+++ src/test/test.java
@@ -0,0 +1,14 @@
+package test;
+
+public class test {
+    
+    public static void main(String[] args) {
+        LinkedList<String> s1 = new LinkedList<>();
+        s1.add("1");
+        s1.add("2");
+        
+        
+        System.out.print(s1.getEntry(1));
+    }
+    
+}
